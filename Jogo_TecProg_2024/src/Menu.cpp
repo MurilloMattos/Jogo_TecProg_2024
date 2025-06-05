383b0e00f29fdfb12706ad044036ed9fa6a99369
Elienai-Benevides <elienai.benevides@hotmail.com>
2025-06-05 15:30:19 -0300

    Criando menu graficamente

--- Jogo_TecProg_2024/src/Menu.cpp
+++ Jogo_TecProg_2024/src/Menu.cpp
@@ -28,8 +28,19 @@ void Menu::atribuir() {
 	figura->loadFromFile("./assets/menu.png");
 	pFigura->setScale(0.7680, 0.500);
 	pFigura->setTexture(*figura);
+	pFigura->setPosition(290,140);
 	
-
+	opcoes = {"Menu","Continuar","Fases","Salvar jogada","Rank","Salvar pontuacao"};
+	coords = {{600,150},{520, 240},{520, 318},{520, 396},{520, 474},{520,552} };
+	tam = {44, 20,20,20,20,20};
+	textos.resize(6);
+        for(size_t i = 0; i < textos.size(); i++) {
+		textos[i].setFont(*fonte);
+		textos[i].setString(opcoes[i]);
+		textos[i].setCharacterSize(tam[i]);
+		//textos[i].setOutlineColor(sf::Color::Red);
+		textos[i].setPosition(coords[i]);
+	}
 }
 void Menu::Executar() {
 	executar();
@@ -41,10 +52,47 @@ pGG->executar(this);
 }
 void Menu::atualizar() {
 	//pJog->Executar();
-	pJog->Atualiza();	
+	pJog->Atualiza();
+	for(int i = 0; i < textos.size(); i++) {
+		pJog->getJanela()->draw(textos[i]);
+	}	
 }
 sf::RenderWindow * Menu::getJanelaMenu() {
 	return menu;
+}
+bool Menu::getPressionado() {
+	return pressionado;
+}
+void Menu::setPressionado(bool valor) {
+	pressionado = valor;
+}
+void Menu::setSelecionado(bool valor) {
+	selecionado = valor;	
+}
+bool Menu::getSelecionado() {
+	return selecionado;
+}
+sf::Vector2i Menu::getPosicaoMouse() {
+	return posicaoMouse;
+}
+void Menu::setPosicaoMouse(sf::Vector2i pos) {
+	posicaoMouse = pos;
+}
+sf::Vector2f  Menu::getCoordsMouse() {
+	return coordMouse;
+}
+void Menu::setCoordsMouse(sf::Vector2f coords) {
+	coordMouse = coords;
+}
+
+void Menu::setPosicaoMenu(int pos) {
+	posicaoMenu  = pos;
+}
+int Menu::getPosicaoMenu() {
+	return posicaoMenu;
+}
+std::vector<sf::Text>* Menu::getTextos() {
+	return &textos;
 }
 void Menu::set_pJog(Jogo *pJ) {
 	if(pJ) {
