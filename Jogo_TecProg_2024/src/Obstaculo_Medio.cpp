fb046f5aeacdb73a086ecaa561f3b956c6b2671e
Elienai-Benevides <elienai.benevides@hotmail.com>
2025-06-22 16:55:03 -0300

    Obstaculo lama adaptando

--- Jogo_TecProg_2024/src/Obstaculo_Medio.cpp
+++ Jogo_TecProg_2024/src/Obstaculo_Medio.cpp
@@ -1,11 +1,13 @@
 #include "Obstaculo_Medio.h"
-
+#include <ctime>
 using namespace Entidades;
 using namespace Personagens;
 using namespace Obstaculos;
 
 Obstaculo_Medio::Obstaculo_Medio():Obstaculo() {
-
+	setFile("./assets/lama.png");
+	atribuirFigura();
+	setar_Pos(230.0, 159.0);
 }
 
 Obstaculo_Medio::~Obstaculo_Medio() {
@@ -13,13 +15,19 @@ Obstaculo_Medio::~Obstaculo_Medio() {
 }
 
 void Entidades::Obstaculos::Obstaculo_Medio::Salvar()
-{
+{	
+
 }
 
 void Obstaculo_Medio::Executar()
 {
+	Desenhar();
+	srand(time(NULL));
+	float random;
+	random = rand()%5+1;
+        setar_Pos(x - random, y);
 }
 
-void Obstaculo_Medio::danificar(Jogador* p)
-{
+void Obstaculo_Medio::obstacular(Entidades::Personagens::Jogador* p) {
+
 }
