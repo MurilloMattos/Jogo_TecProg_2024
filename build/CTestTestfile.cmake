2b6a8bef2067d4f82e6f879f499c738ec2aec4db
kaueicuta2001 <kaueicuta.2001@alunos.utfpr.edu.br>
2025-10-14 21:38:17 -0300

    Primeiro commit do projeto JOGO_TECPROG_2024 em C++ usando CMake como sistema de build no Visual Studio Code.

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: E:/Users/Default/kaue/TecProg/Jogo_TecProg_2024/Jogo_TecProg_2024
+# Build directory: E:/Users/Default/kaue/TecProg/Jogo_TecProg_2024/Jogo_TecProg_2024/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
