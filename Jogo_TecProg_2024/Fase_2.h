1c90e9cf5c8e179e80b9ea736d54d1deabb54743
kaueicuta2001 <kauenadayoshi.2001@gmail.com>
2025-11-19 14:56:16 -0300

    obstaculo Esteira removido das fase 2.

--- Jogo_TecProg_2024/Fase_2.h
+++ Jogo_TecProg_2024/Fase_2.h
@@ -11,8 +11,6 @@ namespace Fases{
 		int num_max_Capitoes;
 		int i,j;
 
-		//Entidades::Obstaculos::Esteira* esteira;
-
 		std::vector<Entidades::Personagens::Inimigo_Capitao*> lista_cap;
 
 	public:
@@ -22,7 +20,6 @@ namespace Fases{
 
 		void Cria_Jogador(float x, float y);
 		void Cria_Obstaculos();
-		//void Cria_Esteira();
 		void Cria_Inimigos();
 		void Cria_Capitao(float x, float y);
 		void Executar();
